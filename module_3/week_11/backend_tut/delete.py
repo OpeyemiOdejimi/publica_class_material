f983e68638cdc6835daa43ab98ab422d4c9c0b28
OpeyemiOdejimi <opeyemiodejimi384@gmail.com>
2025-10-15 15:24:02 +0100

    Update

--- module_3/week_11/backend_tut/delete.py
+++ module_3/week_11/backend_tut/delete.py
@@ -2,36 +2,37 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 data=[]
+
 class BasicAPI(BaseHTTPRequestHandler):
-    def send_data(self, payload, status=201):
+    def send_data(self, payload, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(payload).encode())
 
-    def do_POST(self):
+
+    def do_DELETE(self):
         content_size = int(self.headers.get('Content-Length', 0))
         parsed_data = self.rfile.read(content_size)
-        post_data = json.loads(parsed_data)
-        data.append(post_data) # saving to a database
-        self.send_data([
-            {"message": "Data received successfully"},
-            {"received_data": post_data}
-        ], status=201)
+        delete_data = json.loads(parsed_data)
+
         
+        delete_name = delete_data.get("name")
+        for entry in data:
+            if entry.get("name") == delete_name:
+                data.remove(entry)
+                break
+
+        self.send_data([
+            {"message": "Data deleted successfully"},
+            {"deleted_data": delete_data}
+        ], status=404)
+
 def run():
     server_address = ('', 5000)
     httpd = HTTPServer(server_address, BasicAPI)
     print('Starting server on port 5000...')
     httpd.serve_forever()
-print("Running server...")
+
+print("Delete message abeg!")
 run()
-
-
-
-
-
-
-
-
-
--- module_3/week_11/backend_tut/put.py
+++ module_3/week_11/backend_tut/put.py
@@ -2,36 +2,39 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 data=[]
+
 class BasicAPI(BaseHTTPRequestHandler):
-    def send_data(self, payload, status=201):
+    def send_data(self, payload, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(payload).encode())
 
-    def do_POST(self):
+    def do_PUT(self):
         content_size = int(self.headers.get('Content-Length', 0))
         parsed_data = self.rfile.read(content_size)
-        post_data = json.loads(parsed_data)
-        data.append(post_data) # saving to a database
-        self.send_data([
-            {"message": "Data received successfully"},
-            {"received_data": post_data}
-        ], status=201)
+        put_data = json.loads(parsed_data)
+
         
+        update_name = put_data.get("name")
+        for entry in data:
+            if entry.get("name") == update_name:
+                entry.update(put_data)
+                break
+        else:
+            data.append(put_data)
+
+
+        self.send_data([
+            {"message": "Data updated successfully"},               
+            {"updated_data": put_data}
+        ], status=200)
+
 def run():
     server_address = ('', 5000)
     httpd = HTTPServer(server_address, BasicAPI)
     print('Starting server on port 5000...')
     httpd.serve_forever()
-print("Running server...")
+
+print("The server dey run!")
 run()
-
-
-
-
-
-
-
-
-
